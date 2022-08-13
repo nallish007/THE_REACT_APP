eab68b937451d4cca9ed9b69d695a03d46bdd41d
nallish007 <102135928+nallish007@users.noreply.github.com>
2022-08-13 23:09:26 +0530

    the react app creation

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>vanakam da mappula</p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=0twjvW0c1r0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
